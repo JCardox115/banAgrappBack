ad646215df8e32903b77d26f6cf535806f27dae4
n0zk0r <cardox19@hotmail.com>
2025-04-02 13:09:59 -0500

    ..

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -8,7 +8,7 @@ export const corsConfig: CorsOptions = {
   origin: [
     'https://reportes-nomina.netlify.app',
     'https://67ec2a1563bc005a820f794f--reportes-nomina.netlify.app',
-    'https://reportes-nomina-backend.onrender.com',
+    'https://360c-2800-e2-1e80-2722-9c1b-c274-291c-32c6.ngrok-free.app',
     'http://localhost:4200'
   ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
