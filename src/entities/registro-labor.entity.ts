0904116fa1e1f32ada17005beb8cf4b406d242e6
n0zk0r <cardox19@hotmail.com>
2025-04-28 11:38:57 -0500

    Se agrega el campo centroCostoId en la entidad RegistroLabor y se incluye la validación de id en el DTO de actualización de registros laborales.

--- src/entities/registro-labor.entity.ts
+++ src/entities/registro-labor.entity.ts
@@ -21,6 +21,9 @@ export class RegistroLabor {
   @Column()
   conceptoPagoGrupoLaborId: number;
 
+  @Column()
+  centroCostoId: number;
+
   @ManyToOne(() => ConceptoPagoGrupoLabor)
   conceptoPagoGrupoLabor: ConceptoPagoGrupoLabor;
 
--- src/registros-labor/dto/update-registro-labor.dto.ts
+++ src/registros-labor/dto/update-registro-labor.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRegistroLaborDto } from './create-registro-labor.dto';
+import { IsNotEmpty } from 'class-validator';
 
-export class UpdateRegistroLaborDto extends PartialType(CreateRegistroLaborDto) {} 
+export class UpdateRegistroLaborDto extends PartialType(CreateRegistroLaborDto) {
+    @IsNotEmpty()
+    id: number;
+} 
